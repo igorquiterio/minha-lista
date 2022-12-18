64648837a789db4ca12a2d0ac27db3bbe6268885
Igor da Costa Quiterio <3rd.igor.quiterio@amedigital.com>
2022-12-18 01:58:33 -0300

    fix: url

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -41,7 +41,7 @@ function Lista({ slug, id, itemList }: PageProps) {
   }, [itemList]);
 
   useEffect(() => {
-    axios.post('http://localhost:3000/api/update', {
+    axios.post('https://minha-lista.vercel.app/api/update', {
       id,
       slug,
       items: list,
