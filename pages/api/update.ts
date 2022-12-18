b3cfe8da6acb60db3262fa4f7e8790a8706b48af
Igor da Costa Quiterio <3rd.igor.quiterio@amedigital.com>
2022-12-18 01:42:47 -0300

    fsdfsdfds

--- pages/api/update.ts
+++ pages/api/update.ts
@@ -13,7 +13,7 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
 
     const lista = await minhaLista.updateOne(
       {
-        _id: new ObjectId(id),
+        _id: ObjectId(id),
       },
       {
         $set: {
