dbec73954e9b5190d41a55ee9b88cf63f323ad6f
Igor da Costa Quiterio <3rd.igor.quiterio@amedigital.com>
2022-12-18 01:39:54 -0300

    teste

--- pages/api/update.ts
+++ pages/api/update.ts
@@ -1,6 +1,6 @@
 import clientPromise from '../../lib/mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ObjectId } from 'mongodb';
+import { ObjectId } from 'bson';
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   try {
